31692b25355de7dcf84771c8b1e957355c0da69f
dcaueb78 <caue1017@hotmail.com>
2020-01-15 22:31:03 -0200

    Devs index

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,12 @@ const axios = require('axios');
 const Dev = require('../models/Dev');
 
 module.exports = {
+  async index(req, res) {
+    const devs = await Dev.find();
+
+    return res.json(devs);
+  },
+
   async store (req, res) {
     const { github_username, techs, latitude, longitude } = req.body;
 
