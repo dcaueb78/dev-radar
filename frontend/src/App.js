346873087cc097f400d07ce6446606af1e7573db
dcaueb78 <caue1017@hotmail.com>
2020-01-18 22:52:01 -0200

    Front end first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input
+              name="github_username"
+              id="username_github"
+              required
+            />
+          </div>
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
