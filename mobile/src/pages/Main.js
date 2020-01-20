1d3e7f5e05d6ece16f9fe6fd880597604fd7bd2a
dcaueb78 <caue1017@hotmail.com>
2020-01-20 00:49:49 -0200

    Search techs

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -51,7 +51,7 @@ export default function Main({ navigation }) {
       params: {
         latitude,
         longitude,
-        techs: "ReactJS"
+        techs
       }
     });
 
@@ -110,6 +110,8 @@ export default function Main({ navigation }) {
           placeholderTextColor="#999"
           autoCapitalize="words"
           autoCorrect={false}
+          value={techs}
+          onChangeText={setTechs}
         />
         <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
           <MaterialIcons name="my-location" size={20} color="#FFF" />
