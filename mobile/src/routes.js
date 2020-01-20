7055c7af8742aa3bda401f96acfa84e6bffe0fd1
dcaueb78 <caue1017@hotmail.com>
2020-01-19 23:30:21 -0200

    Header app styles

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,14 +1,36 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-import Main from './pages/Main';
-import Profile from './pages/Profile';
+import Main from "./pages/Main";
+import Profile from "./pages/Profile";
 
 const Routes = createAppContainer(
-  createStackNavigator({
-    Main,
-    Profile,
-  })
+  createStackNavigator(
+    {
+      Main: {
+        screen: Main,
+        navigationOptions: {
+          title: "DevRadar",
+          headerTitleAlign: "center"
+        }
+      },
+      Profile: {
+        screen: Profile,
+        navigationOptions: {
+          title: "Perfil no Github",
+          headerTitleAlign: "center"
+        }
+      }
+    },
+    {
+      defaultNavigationOptions: {
+        headerTintColor: '#FFF',
+        headerStyle: {
+          backgroundColor: "#7D40E7",
+        }
+      }
+    }
+  )
 );
 
 export default Routes;
