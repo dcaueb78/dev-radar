214d1f30f951ab0a3cf60f8e627bce0c799905d3
dcaueb78 <caue1017@hotmail.com>
2020-01-19 23:42:16 -0200

    Get user initial position

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet } from "react-native";
 import MapView from "react-native-maps";
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 export default function Main() {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        })
+      }
+    }
+
+    loadInitialPosition();
+  }, [])
+
   return <MapView style={styles.map} />;
 }
 
