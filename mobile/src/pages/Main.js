c2ca3547d69c7f9ccb5b0e6eacdd5a7dd8b46da3
dcaueb78 <caue1017@hotmail.com>
2020-01-19 23:52:23 -0200

    Dev icon styles

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import { StyleSheet, Image } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync
@@ -36,11 +36,29 @@ export default function Main() {
     return null;
   }
 
-  return <MapView initialRegion={currentRegion} style={styles.map} />;
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -27.2111164, longitude: -49.6374491 }}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: "https://avatars0.githubusercontent.com/u/37030530?s=460&v=4"
+          }}
+        />
+      </Marker>
+    </MapView>
+  );
 }
 
 const styles = StyleSheet.create({
   map: {
     flex: 1
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: "#FFF"
   }
 });
