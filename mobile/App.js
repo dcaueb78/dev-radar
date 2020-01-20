d75ed70d22ba0a44351c88bf1c221d919405825f
dcaueb78 <caue1017@hotmail.com>
2020-01-19 23:20:07 -0200

    Routes config

--- mobile/App.js
+++ mobile/App.js
@@ -1,24 +1,7 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+import Routes from "./src/routes";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
-    </View>
-  );
+  return <Routes />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize:32,
-    color: "#fff"
-  }
-});
