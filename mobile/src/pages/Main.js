c16668c5d38943f7ed5bd12b5dc5d4685591d777
dcaueb78 <caue1017@hotmail.com>
2020-01-19 23:35:38 -0200

    Map view import

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 
 export default function Main() {
-  return <View/>
+  return <MapView style={styles.map} />;
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  }
+});
